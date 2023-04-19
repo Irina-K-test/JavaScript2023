cefe64df0c2f6e6b26d4b8829dc1f54a3ff38a05
Irina-K-test <ifreedom.testing@gmail.com>
2023-04-19 13:44:19 -0700

    6 com

---
+++ les2.js
@@ -0,0 +1,65 @@
+
+// //======= addition(+)
+// let a = 2
+// let b = 3
+// let mysum = a + b
+// document.write(a + b, "<br>")
+
+// //======= change a sign, substruction (-)
+
+// let mySub = 12 - 6
+// let num = 9 *(-1)
+// document.write(mySub, "<br>")
+
+// //======= multiplication (*)
+// let myMultiply = 12 * 6
+// document.write(myMultiply, "<br>")
+
+// //======= division (/)
+
+// //======= modules, reainder of division (even, odd numbers) (%)
+
+// let c = 7
+// let myModulus = c % 2 //(7 / 2 = 3 (1))
+// document.write(myModulus, "<br>")
+
+// //======= incremet and decrement (--, ++)
+
+// let myVariable = 10
+// document.write(myVariable, "<br>")
+
+// document.write(myVariable--, "<br>")
+// document.write(myVariable, "<br>")
+
+// //======= (**) exponentiation
+
+// let myNum = 2 ** 4
+// document.write(myNum, "<br>")
+// console.log(typeof myNum)
+
+//====== boolean 
+
+// let bool1 = true; // true
+// let bool2 = (5>9); // false
+// document.write(bool1, "<br>", bool2)
+// console.log(typeof bool1)
+
+//====== compare 
+
+// let a = "5";
+// let b = -10;
+// let c = "c";
+// let C = "C";
+// let d = "0";
+// let bool = (b != "-10")
+// console.log (a == 5)
+// console.log(bool)
+
+
+//====== logical operators: &&, ||, !
+//|| (or)   &&(or)    ! (NE)
+
+//======= alert() and prompt() 
+// alert ('Welcome!')
+// let inputName = prompt('Please, enter your name!')
+// document.write(`Hello ${inputName} !`)
