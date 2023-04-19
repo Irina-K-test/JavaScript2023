9a2e3a349461ebc7a594d31dba1ed7c291cdd77a
Irina-K-test <ifreedom.testing@gmail.com>
2023-04-19 10:25:37 -0700

    2 commit

---
+++ cupOfCoffee.js
@@ -0,0 +1,73 @@
+let cupSize 
+let smallCup = "small cup"
+let mediumCup = "medium cup"
+let largeCup = "large cup"
+let water 
+let hotWater = "hot water";
+let coldWater = "cold water"
+
+let coffeeType
+let latte = 'Latte';
+let icedCoffee = "Iced Coffee"
+let topping = " ";
+let hotCold = "hot";
+let milkOption = "whole";
+let waterVolume
+
+// make a choice
+coffeeType = icedCoffee
+cupSize = smallCup
+
+//price
+let price
+price = 5;
+
+
+//set recepe ingridients
+// if (coffeeType == icedCoffee) {
+//    water = coldWater
+// } else {
+//     water = hotWater
+// }
+
+// if (cupSize == smallCup) {
+//     waterVolume = '150 ml'
+//     console.log(cupSize, ' - ', price + '$')
+// } else if (cupSize == mediumCup) {
+//     totalPrice = price * 0.5;
+// } else if(cupSize == largeCup) {
+//     totalPrice = price * 0.7;
+// }
+
+//if ()
+
+let step1 = `1. Take a ${cupSize} of coffee`;
+let step2 = "2. Choose type of coffee";
+let step3 = `3. Pour ${hotWater} into the cup`
+let step4 = "4. Pour milk in a cup of coffee"
+let step5 = `5. Press ${coffeeType } button on the coffee machine`;
+let result = " Enjoy your coffee!"
+let totalPrice = ` The price is $${price}`
+//let stepp3 = `3. Press ${coffee } button on the coffee machine`;
+
+
+console.log(step1, `\n`, step2, `\n`, step3,`\n`,step4,`\n`,step5,`\n`,result, `\n`, totalPrice)
+
+
+// Show cup sizes and their prices
+// let cupSize = [smallCup, mediumCup, largeCup]
+// for (let i in cupSize) {
+//     if (cupSize[i] == smallCup) {
+//         console.log(cupSize[i] + ' - ' + price + '$')
+//     }
+//     else if (cupSize == mediumCup) {
+//         console.log(cupSize[i] + '- ' + price * 1.5 + '$')
+//     }
+//     else if (cupSize[i] == largeCup) {
+//         console.log(cupSize[i] + '- ' + price * 2 + '$') 
+//         }
+//         if (i == cupSize.length - 1) {
+//             console.log('\n')
+//         }
+//     }
+
