69e3672954ac6e7ad3479218216b7c0f6053f53b
Irina-K-test <ifreedom.testing@gmail.com>
2023-04-19 10:44:58 -0700

    3 commit

---
+++ hw2.js
@@ -0,0 +1,44 @@
+// let myMonth = 'July';
+// //let month = myMonth.toLowerCase();
+
+// if (myMonth == 'July' || myMonth == 'August') {
+//   console.log ('approved!');
+// } 
+// else console.log ('denied')
+
+// let result 
+// let a = 1;
+// let b = 3;
+// console.log (result = (a + b));
+
+//===== 2 =====
+
+// let num1 = 5
+// let num2 = 9
+// let operator = "-"
+
+// if (operator == "+") {
+//     console.log(num1 + num2)
+// }
+// else if (operator == "-") {
+//     console.log(num1 - num2)
+// }
+// else if (operator == "*") {
+//     console.log(num1 * num2) 
+// }
+// else console.log('Please, enter correct number!')
+
+//===== 3 =====
+
+let dayDistance = 3
+let nightDown = 2
+let distance = 0
+let days = 0
+
+//1
+if ((distance += dayDistance) <= 5) {
+    console.log(` - current distance at the end of the day for day ${days} is ${distance} meter(s)`)
+
+    distance += nightDown
+}
+
